ad2f4b5b2133408a754573fa521ed41b24efdac7
Mohitjadaun2026 <126583966+Mohitjadaun2026@users.noreply.github.com>
2025-06-14 17:19:26 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root', // use your MySQL username
-  password: 'Mohit@123',  // use your MySQL password
-  database: 'ratings_db'
+  host: '217.21.87.103',
+  user: 'u133932327_mohit', // use your MySQL username
+  password: 'Mohit@0824jadaun',  // use your MySQL password
+  database: 'u133932327_mohit'
 });
 
 db.connect((err) => {
